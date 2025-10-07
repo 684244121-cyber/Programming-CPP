400f17e6c4b4a87ed451d911a62f0ac2f3181d8a
684244121-cyber <684244121@mail.pbru.ac.th>
2025-10-07 10:51:31 +0700

    Create Chapter 32-1.cpp

---
+++ Chapter32-36/Chapter 32-1.cpp
@@ -0,0 +1,13 @@
+void myfunction();  //function declaration
+source.cpp:
+#include "myheader.h" //include the header
+#include <iostream>
+int main()
+{
+    myfunction();
+}
+// function definition
+void myfunction()
+{
+    std::cout << "Hello World from multiple files.";
+}
